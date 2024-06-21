6c06732d790b55ad494fb7fb13de576912805f0f
ErikCasas <santicasas667@gmail.com>
2024-06-21 06:53:38 -0500

    Move the button to make the lab clearer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,6 @@ export const NavBar = () => {
 				src='https://media.ada-school.org/5fcd3ac12b22eab4d301d819/5fcd49a07ffe7b324996b784/ada-logo-cfdb7c7b-1791-408f-b5b4-240b22bd1653.png?version=2'
 				alt='ada-logo'
 			/>
-			<button>{true ? '☀️' : '🌕'}</button>
 			<ul>
 				<li>
 					<Link to={'/'}>Inicio</Link>
@@ -16,6 +15,7 @@ export const NavBar = () => {
 					<Link to={'/news'}>Noticias</Link>
 				</li>
 			</ul>
+			<button>{true ? '☀️' : '🌕'}</button>
 		</nav>
 	)
 }
